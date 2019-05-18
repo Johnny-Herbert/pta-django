4cf1ca797a46f28b2557333c8063b436631e6d1d
Johnny Herbert Muniz Nunes <jhmn@ug3c16.windows.cin.ufpe.br>
2019-05-18 12:03:58 -0300

    Aula de django dia 18-05

--- ptaDjango/core/models.py
+++ ptaDjango/core/models.py
@@ -4,8 +4,9 @@ from django.db import models
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length = 100, null = False, blank = False) #nao pode ser nulo e vazio
+    title = models.CharField(max_length = 100, null = False, blank = False, verbose_name = "Titulo") #nao pode ser nulo e vazio
     text = models.TextField(null = False, blank = False) #nao pode ser nulo e vazio
+    image = models.ImageField('Imagem', upload_to = 'institute/', null = False, blank = False)
     created_date = models.DateTimeField(auto_now_add=True)
     upudated_date = models.DateTimeField(auto_now = True)
 
--- ptaDjango/ptaDjango/urls.py
+++ ptaDjango/ptaDjango/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import HomeView
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='index')
-]
+    path('', include(('core.urls'), namespace='index')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
